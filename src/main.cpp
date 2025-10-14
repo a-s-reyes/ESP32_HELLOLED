e149e9e8a7b42ac9c123898cd7b4d70a74f868f3
Andrean Reyes <andreanreyes.ar@gmail.com>
2025-10-13 21:43:01 -0500

    ardu fade

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,23 @@
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "driver/ledc.h"
-#include <iostream>
+#include <Arduino.h>
 
-class FadingLED {
-public:
-    FadingLED(int pin, ledc_channel_t channel, int freq = 5000) 
-        : pin(pin), channel(channel), brightness(0), fadeAmount(1) 
-    {
-        ledc_timer_config_t timerConfig = {
-            .speed_mode = LEDC_LOW_SPEED_MODE,
-            .duty_resolution = LEDC_TIMER_8_BIT,
-            .timer_num = LEDC_TIMER_0,
-            .freq_hz = freq,
-            .clk_cfg = LEDC_AUTO_CLK
-        };
-        ledc_timer_config(&timerConfig);
+const int ledPin = 26; 
+int brightness = 0; 
+int fadeAmount = 5; 
+const int ledChannel = 0; 
 
-        ledc_channel_config_t channelConfig = {
-            .gpio_num = pin,
-            .speed_mode = LEDC_LOW_SPEED_MODE,
-            .channel = channel,
-            .timer_sel = LEDC_TIMER_0,
-            .duty = 0,
-            .hpoint = 0
-        };
-        ledc_channel_config(&channelConfig);
-    }
+void setup() {
+  ledcSetup(ledChannel, 5000, 8);
 
-    void update() {
-        ledc_set_duty(LEDC_LOW_SPEED_MODE, channel, brightness);
-        ledc_update_duty(LEDC_LOW_SPEED_MODE, channel);
+  ledcAttachPin(ledPin, ledChannel);
+}
 
-        brightness += fadeAmount;
-        if (brightness <= 0 || brightness >= 255) fadeAmount = -fadeAmount;
-    }
+void loop() {
+  ledcWrite(ledChannel, brightness);
 
-private:
-    int pin;
-    ledc_channel_t channel;
-    int brightness;
-    int fadeAmount;
-};
+  brightness += fadeAmount;
+  if (brightness <= 0 || brightness >= 255) {
+    fadeAmount = -fadeAmount;  
+  }
 
-extern "C" void app_main() {
-    FadingLED led(26, LEDC_CHANNEL_0);
-
-    while (true) {
-        led.update();
-        vTaskDelay(20 / portTICK_PERIOD_MS);
-    }
+  delay(50); 
 }
